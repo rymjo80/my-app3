0b8df2c0ca4d5ff234f03fdc6104474d79f8c6c6
Ryan Johnson <ryanmjohnson03@gmail.com>
2022-08-19 22:17:14 -0400

    Initial

---
+++ src/test/java/edu/au/cpsc/AppTest.java
@@ -0,0 +1,17 @@
+package edu.au.cpsc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() { assertTrue( true ); }
+}
